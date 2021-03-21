f9e169094440e085d6edfcab60167081d3023f2d
Ovidiu Lipianu <ovidexter4@gmail.com>
2021-03-21 18:52:42 +0200

    Added interaction for customers
    they can wait or leave

--- AtelierAuto/Angajat.h
+++ AtelierAuto/Angajat.h
@@ -4,13 +4,27 @@
 
 #ifndef ATELIERAUTO_ANGAJAT_H
 #define ATELIERAUTO_ANGAJAT_H
-
-
 #include <string>
+#include <vector>
+#include <iostream>
+
+
 struct data{
     int zi,luna,an;
 };
-
+struct coada_angajat{
+    int standard;
+    int camion,autobuz;
+};
+struct car_time{
+    int time;
+    std::string type;
+};
+struct extra{
+    std::vector<car_time> camion;
+    std::vector<car_time> autobuz;
+    std::vector<car_time> standard;
+};
 class Angajat {
 protected:
     int ID;
@@ -19,10 +33,19 @@ protected:
     float coeficient;
     data nastere;
     data angajare;
+    coada_angajat working;
+    std::vector<car_time>  waiting;
+    extra  extra_waiting;
+    static int timp_lucru_random()
+    {int v=rand()%10+1;
+        return v;
+    }
 public:
     static int next_id;
-
     Angajat(std::string Nume,std::string Prenume,data nastere,data angajare) {
+        working.standard=0;
+        working.camion=0;
+        working.autobuz=0;
         this->setId(next_id);
         next_id++;
         this->setNume(Nume);
@@ -30,7 +53,106 @@ public:
         this->setNastere(nastere);
         this->setAngajare(angajare);
     }
+    bool check_available(std::string car_type){
+        if(car_type=="standard"&& this->working.standard<3){
+            this->working.standard++;
+            int t=timp_lucru_random();
+            this->waiting.push_back((car_time){t,"standard"});
+            return true;
+        }
+        else if(car_type=="autobuz"&& this->working.autobuz==0){
+            this->working.autobuz++;
+            int t=timp_lucru_random();
 
+            this->waiting.push_back((car_time){t,"autobuz"});
+            return true;
+        }
+        else if(car_type=="camion"&& this->working.camion==0){
+            this->working.camion++;
+            int t=timp_lucru_random();
+            this->waiting.push_back((car_time){t,"camion"});
+            return true;
+        }
+        this->wait_here(car_type);
+        return false;
+    }
+    bool isBusy(std::string car_type){
+        if(car_type=="standard"&& this->working.standard==3){
+            return true;
+        }
+        else if(car_type=="autobuz"&& this->working.autobuz==1){
+            return true;
+        }
+        else if(car_type=="camion"&& this->working.camion==1){
+            return true;
+        }
+        return false;
+    }
+    void wait_here(std::string car_type){
+        if(car_type=="standard"){
+            extra_waiting.standard.push_back((car_time){timp_lucru_random(),car_type});
+        }
+        else if(car_type=="autobuz"){
+            extra_waiting.autobuz.push_back((car_time){timp_lucru_random(),car_type});
+        }
+        else if(car_type=="camion"){
+            extra_waiting.camion.push_back((car_time){timp_lucru_random(),car_type});
+        }
+    }
+    void afisare_coada(){
+        for(auto w:waiting)
+        {std::cout<<w.type<<":"<<w.time<<" minutes- ";
+        }
+        for(auto w:extra_waiting.standard){
+            std::cout<<w.time<<" ";
+        }
+        for(auto w:extra_waiting.autobuz){
+            std::cout<<w.time<<" ";
+        }
+        for(auto w:extra_waiting.camion){
+            std::cout<<w.time<<" ";
+        }
+
+    }
+    void pass_time(){
+        if(waiting.size()>0) {
+            waiting[0].time--;
+            if (waiting[0].time <= 0) {
+                if(waiting[0].type=="standard"){
+                    working.standard--;
+                    if(extra_waiting.standard.size()>0){
+                        waiting.push_back(extra_waiting.standard.front());
+                        extra_waiting.standard.erase( extra_waiting.standard.begin());
+                    }
+                }
+                else if(waiting[0].type=="autobuz"){
+                    working.autobuz--;
+                    if(extra_waiting.autobuz.size()>0){
+                        waiting.push_back(extra_waiting.autobuz.front());
+                        extra_waiting.autobuz.erase(extra_waiting.autobuz.begin());
+
+                    }
+                }
+                else if(waiting[0].type=="camion"){
+                    working.camion--;
+                    if(extra_waiting.camion.size()>0){
+                        waiting.push_back(extra_waiting.camion.front());
+                        extra_waiting.camion.erase(extra_waiting.camion.begin());
+
+                    }
+                }
+                waiting.erase(waiting.begin());
+            }
+        }
+    }
+
+    int waiting_time(){
+        int t=0;
+        for(auto car:waiting){
+            t+=car.time;
+        }
+        return t;
+    }
     Angajat(){
 
     }
--- AtelierAuto/main.cpp
+++ AtelierAuto/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <stdlib.h>
 #include <ctime>
 #include "Angajat.h"
 #include "Masina.h"
 int Angajat::next_id=0;
+
 
 int age(data today,data birth)
 {
@@ -34,141 +36,253 @@ int age(data today,data birth)
     {d = pd - bd;}
     return y;
 }
-
+bool isFull(std::vector<Angajat*> angajati,std::string type){
+    bool flag=1;
+    for(auto ang:angajati){
+        if(!ang->isBusy(type))flag=0;
+    }
+    return flag;
+}
 int main() {
+
     std::time_t t = std::time(0);   // get time now
     std::tm* now = std::localtime(&t);
     data azi;
     azi.zi=now->tm_mday;
     azi.luna=now->tm_mon + 1;
     azi.an=now->tm_year + 1900;
+//    std::vector<Masina*> extra_list;
+    extra  extra_list_global;
 
     std::vector<Masina*> m;
 //    m.push_back(new Standard(0,500000,2020,1,1));
 //    m.push_back(new Camion(1,500000,2009,1,2));
 //    m.push_back(new Autobuz(2,500000,2019,1,10));
 //    std::cout<<m[0]->getAsigurare(1)<<" "<<m[1]->getAsigurare(1)<<" "<<m[2]->getAsigurare(1);
-
     std::vector<Angajat*> a;
+
     a.push_back(new Director("Lipianu","Ovidiu",azi,data{9,12,1990}));
     a.push_back(new Mecanic("Lipianu","Florin",azi,data{9,12,1990}));
-    a.push_back(new Asistent("Stan","Florin",azi,data{9,12,1990}));
+    a.push_back(new Asistent("Stan","Ana",azi,data{9,12,1990}));
 
-//    while (1){
-//        std::string command;
-//        std::cin>>command;
-//
-//        if(command=="exit")break;
-//
-//        if(command=="adauga"){
-//            std::string nume,prenume;
-//            data day,bday;
-//
-//            while(!(nume.size()>0&&nume.size()<30))
-//            {std::cout<<"introduceti numele(maxim 30 de caractere):";
-//                std::cin>>nume;}
-//
-//            while(!(prenume.size()>0&&prenume.size()<30))
-//            {std::cout<<"introduceti prenumele(maxim 30 de caractere):";
-//                std:: cin>>prenume;}
-//
-//            std::cout<<"introduceti data nasterii(zi luna an):\n";
-//            std::cin>>bday.zi>>bday.luna>>bday.an;
-//            while(age(azi,bday)<18)
-//            {std::cout<<"Angajatul trebuie să aibă peste 18 ani\n";
-//                std::cin>>bday.zi>>bday.luna>>bday.an;
-//            }
-//
-//            std::cout<<"introduceti data angajarii(zi luna an):\n";
-//            std::cin>>day.zi>>day.luna>>day.an;
-//            while(age(azi,day)<0)
-//            {std::cout<<"Data angajării nu poate fi o dată din viitor.\n";
-//             std::cout<<"introduceti data angajarii(zi luna an):\n";
-//                std::cin>>day.zi>>day.luna>>day.an;
-//            }
-//            std::string job;
-//            std::cout<<"introduceti funtia(Director/Mecanic/Asistent):\n";
-//            std::cin>>job;
-//            if(job=="Director"){
-//                a.push_back(new Director(nume,prenume,bday,day));
-//            }
-//            else if (job=="Mecanic"){a.push_back(new Mecanic(nume,prenume,bday,day));
-//                }
-//            else if(job=="Asistent"){a.push_back(new Asistent(nume,prenume,bday,day));
-//                }
-//            else {
-//
-//            }
-//        }
-//
-//        if(command=="afiseaza"){
-//            for(auto ang:a){
-//                std::cout<<ang->getPrenume()<<" "<<ang->getNume()<<"\n";
-//            }
-//        }
-//        if(command=="salariu"){
-//            int id;
-//            bool found=0;
-//            while(!found) {
-//                std::cout << "introducere id angajat:";
-//                std::cin >> id;
-//                for (auto ang:a) {
-//                    if (ang->getID() == id) {
-//                        found=1;
-//                        std::cout << 1000.0 * age(azi, ang->getAngajare()) * ang->getCoeficient() << "\n";
-//                    }
-//                }
-//                if(!found)std::cout<<"id invalid\n";
-//            }
-//        }
-//        if (command=="sterge"){
-//            int id;
-//            bool found=0;
-//            while(!found) {
-//                std::cout << "introducere id angajat:";
-//                std::cin >> id;
-//                int i=0;
-//                for (auto ang:a) {
-//                    if (ang->getID() == id) {
-//                        found=1;
-//                        a.erase(a.begin()+i);
-//                    }
-//                    i++;
-//                }
-//                if(!found)std::cout<<"id invalid\n";
-//            }
-//        }
-//        if(command=="edit"){
-//            int id;
-//            bool found=0;
-//            while(!found) {
-//                std::cout << "introducere id angajat:";
-//                std::cin >> id;
-//                for (auto ang:a) {
-//                    if (ang->getID() == id) {
-//                        found=1;
-//                        std::string camp;
-//                        std::cout<<"Introduce campul pe care doresti sa il modifici:\n";
-//                        std::cin>>camp;
-//                        if(camp=="nume"){
-//                            std::string nume;
-//                            std::cout<<"Nume nou:\n";
-//                            std::cin>>nume;
-//                            ang->setNume(nume);
-//                        }else if(camp=="prenume"){
-//                            std::string nume;
-//                            std::cout<<"Prenume nou:\n";
-//                            std::cin>>nume;
-//                            ang->setPrenume(nume);
-//                        }else {
-//                            //de adaugat pentru data de nastere si data de angajare
-//                        }
-//                    }
-//                }
-//                if(!found)std::cout<<"id invalid\n";
-//            }
-//        }
-//    }
+    while (1){
+//        std::cout<<isFull(a,"standard")<<"\n";
+        for(auto ang:a){
+            ang->pass_time();
+            if(ang->check_available("standard"));
+            std::cout<<"waiting time for "<<ang->getPrenume()<<" "<<ang->waiting_time()<<" queue: ";
+            ang->afisare_coada();
+            std::cout<<ang->isBusy("standard")<<"\n";
+            ang->wait_here("standard");
+        }
+
+        std::string command;
+        std::cin>>command;
+
+        if(command=="exit")break;
+        if(command=="wait")continue;
+        if(command=="adauga"){
+            std::string nume,prenume;
+            data day,bday;
+
+            while(!(nume.size()>0&&nume.size()<30))
+            {std::cout<<"introduceti numele(maxim 30 de caractere):";
+                std::cin>>nume;}
+
+            while(!(prenume.size()>0&&prenume.size()<30))
+            {std::cout<<"introduceti prenumele(maxim 30 de caractere):";
+                std:: cin>>prenume;}
+
+            std::cout<<"introduceti data nasterii(zi luna an):\n";
+            std::cin>>bday.zi>>bday.luna>>bday.an;
+            while(age(azi,bday)<18)
+            {std::cout<<"Angajatul trebuie să aibă peste 18 ani\n";
+                std::cin>>bday.zi>>bday.luna>>bday.an;
+            }
+
+            std::cout<<"introduceti data angajarii(zi luna an):\n";
+            std::cin>>day.zi>>day.luna>>day.an;
+            while(age(azi,day)<0)
+            {std::cout<<"Data angajării nu poate fi o dată din viitor.\n";
+             std::cout<<"introduceti data angajarii(zi luna an):\n";
+                std::cin>>day.zi>>day.luna>>day.an;
+            }
+            std::string job;
+            std::cout<<"introduceti funtia(Director/Mecanic/Asistent):\n";
+            std::cin>>job;
+            if(job=="Director"){
+                a.push_back(new Director(nume,prenume,bday,day));
+            }
+            else if (job=="Mecanic"){a.push_back(new Mecanic(nume,prenume,bday,day));
+                }
+            else if(job=="Asistent"){a.push_back(new Asistent(nume,prenume,bday,day));
+                }
+            else {
+
+            }
+        }
+
+        if(command=="afiseaza"){
+            for(auto ang:a){
+                std::cout<<ang->getPrenume()<<" "<<ang->getNume()<<"\n";
+            }
+        }
+        if(command=="salariu"){
+            int id;
+            bool found=0;
+            while(!found) {
+                std::cout << "introducere id angajat:";
+                std::cin >> id;
+                for (auto ang:a) {
+                    if (ang->getID() == id) {
+                        found=1;
+                        std::cout << 1000.0 * age(azi, ang->getAngajare()) * ang->getCoeficient() << "\n";
+                    }
+                }
+                if(!found)std::cout<<"id invalid\n";
+            }
+        }
+        if (command=="sterge"){
+            int id;
+            bool found=0;
+            while(!found) {
+                std::cout << "introducere id angajat:";
+                std::cin >> id;
+                int i=0;
+                for (auto ang:a) {
+                    if (ang->getID() == id) {
+                        found=1;
+                        a.erase(a.begin()+i);
+                    }
+                    i++;
+                }
+                if(!found)std::cout<<"id invalid\n";
+            }
+        }
+        if(command=="edit"){
+            int id;
+            bool found=0;
+            while(!found) {
+                std::cout << "introducere id angajat:";
+                std::cin >> id;
+                for (auto ang:a) {
+                    if (ang->getID() == id) {
+                        found=1;
+                        std::string camp;
+                        std::cout<<"Introduce campul pe care doresti sa il modifici:\n";
+                        std::cin>>camp;
+                        if(camp=="nume"){
+                            std::string nume;
+                            std::cout<<"Nume nou:\n";
+                            std::cin>>nume;
+                            ang->setNume(nume);
+                        }else if(camp=="prenume"){
+                            std::string nume;
+                            std::cout<<"Prenume nou:\n";
+                            std::cin>>nume;
+                            ang->setPrenume(nume);
+                        }else {
+                            //de adaugat pentru data de nastere si data de angajare
+                        }
+                    }
+                }
+                if(!found)std::cout<<"id invalid\n";
+            }
+        }
+        if(command=="atelier"){
+            if(a.size()>0) {
+                int id_Car, km_Car, an_Car;
+                bool diesel_Car;
+                std::cout << "introduceti datele masinii:\n";
+                std::cout << "introduceti id:\n";
+                std::cin >> id_Car;
+                std::cout << "introduceti kilometraj:\n";
+                std::cin >> km_Car;
+                std::cout << "introduceti anul de fabricatie:\n";
+                std::cin >> an_Car;
+                std::cout << "este diesel?(da/nu):\n";
+                std::string msg;
+                std::cin >> msg;
+                if (msg == "da")diesel_Car = true;
+                else if (msg == "nu")diesel_Car = false;
+                std::cout << "Care este tipul masinii?(standard/autobuz/camion):\n";
+                std::cin >> msg;
+                if (msg == "standard") {
+                    bool transmisie;
+                    std::cout << "Care este tipul de transmisie?(1 pentru manual/0 pentru automat):\n";
+                    std::cin >> transmisie;
+                    if (transmisie) {
+                        m.push_back(new Standard(id_Car, km_Car, an_Car, diesel_Car,transmisie));
+                    } else {
+
+                    }
+                } else if (msg == "autobuz") {
+                    int locuri;
+                    std::cout << "Care este numarul de locuri?:\n";
+                    std::cin >> locuri;
+                } else if (msg == "camion") {
+                    int tonaj;
+                    std::cout << "Care este tonajul?:\n";
+                    std::cin >> tonaj;
+                }
+                std::cout<<"Pentru a continua (solicita):";
+                std::string message;
+                std::cin>>message;
+                if(message == "solicita") {
+                    if(isFull(a,msg)){
+                        std::string da_nu;
+                        std::cout<<"Toti angajatii sunt ocupati. Doresti sa astepti?(da/nu)\n";
+                        std::cin>>da_nu;
+                        if(da_nu=="da"){
+                            if(msg=="camion")extra_list_global.camion.push_back((car_time){rand()%10+1,msg});
+                            else if(msg=="standard")extra_list_global.standard.push_back((car_time){rand()%10+1,msg});
+                            else if(msg=="autobuz")extra_list_global.autobuz.push_back((car_time){rand()%10+1,msg});
+                        }
+                        else if(da_nu=="nu"){std::cout<<"La revedere!\n"; continue;}
+                    }
+                    else{
+                        std::string mesaj;
+                        std::cout<<"Doriti un angajat special/primul disponibil(special/primul):\n";
+                        std::cin>>mesaj;
+                        if(mesaj=="primul"){
+                            for(auto angj:a){
+                                if(!angj->isBusy(msg))angj->check_available(msg);
+                            }
+                        }
+                        else if(mesaj=="special"){
+                            std::cout<<"Introduceti id angajat:";
+                            int id_ang;
+                            std::cin>>id_ang;
+                            for(auto ang:a){
+                                if(ang->getID()==id_ang){
+                                    if(ang->isBusy(msg)){
+                                        std::string msg_ang;
+                                        std::cout<<"Angajatul este ocupat!\n"<<"(asteapta/pleaca/primul):\n";
+                                        if(msg_ang=="asteapta"){
+                                            ang->wait_here(msg);
+                                        }
+                                        else if(msg_ang=="primul"){
+                                                for(auto angj:a){
+                                                    if(!angj->isBusy(msg))angj->check_available(msg);
+                                                }
+                                        }
+                                        else if(msg_ang=="pleaca"){std::cout<<"La revedere!\n"; continue;}
+
+                                    }
+                                    else{
+                                        ang->check_available(msg);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else{
+                std::cout<<"Atelierul este inchis!\n";
+            }
+        }
+    }
 
     return 0;
 }
